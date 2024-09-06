a5453f58e64f4b00d3aee844b530e09ea6ffe6d9
kentcarloestillore <152847075+kentcarloestillore@users.noreply.github.com>
2024-09-06 08:50:09 +0800

    Update resume.tsx

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -6,7 +6,6 @@ import { RESUME_TEXT_1, RESUME_TEXT_2 } from 'constants/common';
 
 // Jsons
 import Profiles from 'jsons/profiles.json';
-import Experiences from 'jsons/experiences.json';
 
 // Styles
 import './resume.scss';
