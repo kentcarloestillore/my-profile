aa4e8fb39863aea58ae02be7f5b1e841d5e5fdd6
kentcarloestillore <kentcarloestillore@gmail.com>
2024-09-06 08:16:50 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,23 @@
+export const NAME = 'Kent Carlo J. Estillore';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Motivated student with experience in writing, testing, and debugging software code to develop functional applications. Skilled in programming languages, algorithms, data structures, and software development methodologies, with a strong foundation in practical coding and problem-solving.';
+export const ABOUT_TEXT_2 =
+  'Currently studying with experience in developing and maintaining web applications across both frontend and backend. Works collaboratively with teams to design, implement, and deploy scalable solutions that align with project goals.';
+export const ABOUT_TEXT_3 =
+  'Versatile student skilled in both frontend and backend development, capable of building complete web applications from start to finish. Strong understanding of various programming languages, frameworks, and databases.';
+export const ABOUT_TEXT_4 =
+  `Hi, I'm Kent Carlo Estillore, an enthusiastic IT student with a passion for technology and innovation. Currently, I'm pursuing my degree in Information Technology, where I'm honing my skills in software development, web design, and network administration.`;
+export const ABOUT_TEXT_5 =
+  `I have a strong interest in learning new technologies and applying them to solve real-world problems. My journey in IT has been driven by curiosity and a desire to create impactful digital solutions. From coding small projects to collaborating on larger initiatives, each experience has taught me valuable lessons.`;
+export const ABOUT_TEXT_6 =
+  `Outside of my academic pursuits, I enjoy exploring the latest trends in tech, engaging in online communities, and participating in hackathons. These activities not only enhance my technical skills but also allow me to connect with like-minded individuals and expand my professional network.`;
+export const ABOUT_TEXT_7 =
+  `My goal is to continue growing as a versatile IT professional, ready to tackle challenges and contribute to innovative projects. I am always open to new opportunities and eager to learn more about the dynamic world of information technology.`;
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Aspiring Software Developer with a strong foundation in web and application development. Currently pursuing a Bachelor’s in Information Technology, with hands-on experience in coding with PHP and Laravel, as well as JavaScript frameworks including Vue, React, and jQuery. Proficient in Java and Dart, with practical skills in building cross-platform applications using Flutter. Eager to apply knowledge and grow in a dynamic development environment..';
+export const RESUME_TEXT_2 =
+  'BSIT student with hands-on experience in PHP (Laravel), JavaScript (Vue, React, jQuery), Java, and Dart (Flutter), eager to grow as a software developer.';
